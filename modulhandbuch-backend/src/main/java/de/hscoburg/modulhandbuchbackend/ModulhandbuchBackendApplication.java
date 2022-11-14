039335314fd6cc7e7c9f63e5d3ee8f274148e459
Ganko Pi <ganko-pi@outlook.de>
2022-11-14 20:08:08 +0100

    fix: add configurations for use with tomcat

--- modulhandbuch-backend/src/main/java/de/hscoburg/modulhandbuchbackend/ModulhandbuchBackendApplication.java
+++ modulhandbuch-backend/src/main/java/de/hscoburg/modulhandbuchbackend/ModulhandbuchBackendApplication.java
@@ -2,9 +2,10 @@ package de.hscoburg.modulhandbuchbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ModulhandbuchBackendApplication {
+public class ModulhandbuchBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModulhandbuchBackendApplication.class, args);
