300d2385922806871f3ede8643b95980fed09c46
Simon Vetter <simon.vetter@stud.hs-coburg.de>
2022-11-20 16:29:05 +0100

    feat: a4 test

--- modulhandbuch-test/src/test/java/a4/a4_systemtest.java
+++ modulhandbuch-test/src/test/java/a4/a4_systemtest.java
@@ -1,6 +1,38 @@
 package a4;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class a4_systemtest {
-	
+
+    private WebDriver driver;
+    @BeforeTest
+    public void openWebsite() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("disable-gpu");
+        driver = new ChromeDriver(chromeOptions);
+            
+//		driver = new ChromeDriver();
+        driver.get("https://85.214.225.164/dev");
+//		driver.manage().window().maximize();
+        driver.findElement(By.id("details-button")).click();
+        driver.findElement(By.id("proceed-link")).click();
+    }
+    
+
+
+
+
 
 }
