97003a60d937e629fc1cf775357e32d23d490282
Quynh Anh Giang <ngu8763s>
2022-11-30 10:11:35 +0100

    add a4 systemtest

--- modulhandbuch-test/src/test/java/a4/a4_systemtest.java
+++ modulhandbuch-test/src/test/java/a4/a4_systemtest.java
@@ -58,7 +58,7 @@ public class a4_systemtest {
 
 		Assert.assertEquals(true, true);
 	}
-	
+
 	@Test
 	public void S_F_A4T01() {
 		WebElement dialog = driver.findElement(By.xpath("//div[@role='dialog']"));
@@ -66,22 +66,211 @@ public class a4_systemtest {
 		boolean result = !dialog.equals(null) && dialogTitle.contentEquals("Neues Modul erstellen");
 		Assert.assertEquals(result, true);
 	}
+
+	@Test
+
+	public void S_F_A4T02() {
+		WebElement modulbezeichnung = driver.findElement(By.xpath("//input[@formcontrolname='moduleName']"));
+		boolean modulbezeichnungRequired = Boolean.valueOf(modulbezeichnung.getAttribute("required"));
+
+		boolean result = modulbezeichnungRequired;
+		Assert.assertEquals(result, true);
+	}
+
+	@Test
+
+	public void S_F_A4T03() {
+		WebElement kuerzel = driver.findElement(By.xpath("//input[@formcontrolname='abbreviation']"));
+		boolean kuerzelRequired = Boolean.valueOf(kuerzel.getAttribute("required"));
+
+		boolean result = kuerzelRequired;
+		Assert.assertEquals(result, true);
+	}
+
+	@Test
+
+	public void S_F_A4T04() {
+		WebElement kategorie = driver.findElement(By.xpath("//select[@formcontrolname='category']"));
+		boolean kategorieRequired = Boolean.valueOf(kategorie.getAttribute("required"));
+
+		boolean result = kategorieRequired;
+		Assert.assertEquals(result, true);
+	}
 	
 	@Test
-	public void S_F_A4T02() {
-		WebElement submitCloseButton = driver.findElement(By.id("bt-submit-close"));
-		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		submitCloseButton.click();
+
+	public void S_F_A4T05() {
+		WebElement sws = driver.findElement(By.xpath("//input[@formcontrolname='sws']"));
+		boolean swsRequired = Boolean.valueOf(sws.getAttribute("required"));
+		
+		boolean result = swsRequired;
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+
+	public void S_F_A4T06() {
+		WebElement ects = driver.findElement(By.xpath("//input[@formcontrolname='ects']"));
+		boolean ectsRequired = Boolean.valueOf(ects.getAttribute("required"));
+		
+		boolean result = ectsRequired;
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+
+	public void S_F_A4T07() {
+		WebElement fachsemester = driver.findElement(By.xpath("//input[@formcontrolname='semester']"));
+		boolean fachsemesterRequired = Boolean.valueOf(fachsemester.getAttribute("required"));
+		
+		boolean result = fachsemesterRequired;
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+
+	public void S_F_A4T08() {
+		WebElement angebotsturnus = driver.findElement(By.xpath("//select[@formcontrolname='cycle']"));
+		boolean angebotsturnusRequired = Boolean.valueOf(angebotsturnus.getAttribute("required"));
+		
+		boolean result = angebotsturnusRequired;
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+
+	public void S_F_A4T09() {
+		WebElement dauerDesModuls = driver.findElement(By.xpath("//select[@formcontrolname='duration']"));
+		boolean dauerDesModulsRequired = Boolean.valueOf(dauerDesModuls.getAttribute("required"));
+		
+		boolean result = dauerDesModulsRequired;
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+
+	public void S_F_A4T10() {
+		WebElement dozent = driver.findElement(By.xpath("//div[@class='p-element p-multiselect-label-container ng-tns-c60-2']"));
+		boolean dozentRequired = Boolean.valueOf(dozent.getAttribute("required"));
+		
+		boolean result = dozentRequired;
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+
+	public void S_F_A4T11() {
+		WebElement sprache = driver.findElement(By.xpath("//select[@formcontrolname='language']"));
+		boolean spracheRequired = Boolean.valueOf(sprache.getAttribute("required"));
+		
+		boolean result = spracheRequired;
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+
+	public void S_F_A4T12() {
+		WebElement pruefungsleistungen = driver.findElement(By.xpath("//input[@formcontrolname='examType']"));
+		boolean pruefungsleistungenRequired = Boolean.valueOf(pruefungsleistungen.getAttribute("required"));
+		
+		boolean result = pruefungsleistungenRequired;
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+
+	public void S_F_A4T13() {
+		WebElement mutterschutz = driver.findElement(By.xpath("//select[@formcontrolname='maternityProtection']"));
+		boolean mutterschutzRequired = Boolean.valueOf(mutterschutz.getAttribute("required"));
+		
+		boolean result = mutterschutzRequired;
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+	
+	public void S_F_A4T14() {
+		WebElement kuerzel = driver.findElement(By.xpath("//input[@formcontrolname='abbreviation']"));
+		short kuerzelMaxLength = Short.valueOf(kuerzel.getAttribute("maxlength"));
+		
+		boolean result = (kuerzelMaxLength == 10);
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+	
+	public void S_F_A4T15() {
+	WebElement sws = driver.findElement(By.xpath("//input[@formcontrolname='sws']"));
+		short swsMin = Short.valueOf(sws.getAttribute("min"));
+		
+		boolean result = (swsMin == 0);
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+	
+	public void S_F_A4T16() {
+	WebElement sws = driver.findElement(By.xpath("//input[@formcontrolname='sws']"));
+		short swsMax = Short.valueOf(sws.getAttribute("max"));
+		
+		boolean result = (swsMax == 40);
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+	
+	public void S_F_A4T17() {
+		WebElement ects = driver.findElement(By.xpath("//input[@formcontrolname='ects']"));
+		short ectsMin = Short.valueOf(ects.getAttribute("min"));
+		
+		boolean result = (ectsMin == 1);
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+	
+	public void S_F_A4T18() {
+		WebElement ects = driver.findElement(By.xpath("//input[@formcontrolname='ects']"));
+		short ectsMax = Short.valueOf(ects.getAttribute("max"));
+		
+		boolean result = (ectsMax == 30);
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+	
+	public void S_F_A4T19() {
+		WebElement fachsemester = driver.findElement(By.xpath("//input[@formcontrolname='semester']"));
+		short fachsemesterMin = Short.valueOf(fachsemester.getAttribute("min"));
+		
+		boolean result = (fachsemesterMin == 1);
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+	
+	public void S_F_A4T20() {
+		WebElement fachsemester = driver.findElement(By.xpath("//input[@formcontrolname='semester']"));
+		short fachsemesterMax = Short.valueOf(fachsemester.getAttribute("max"));
+		
+		boolean result = (fachsemesterMax == 7);
+		Assert.assertEquals(result, true);
+	}
+	
+	@Test
+	
+	public void S_F_A4T21() {
+		driver.findElement(By.id("btn-remove-variations")).click();
 		boolean result = true;
-	    try 
-	    { 
-	        driver.switchTo().alert(); 
-	    }   
-	    catch (NoAlertPresentException NoAlertEx) 
-	    { 
-            result = false;
-	    }
-	    Assert.assertEquals(result, true);
+		
+		try{
+			Alert alert = driver.switchTo().alert();
+			alert.accept();
+		}
+		catch(NoAlertPresentException noAlertEx) {
+			result = false;
+		}
+		Assert.assertEquals(result, true);		
 	}
 
 }
