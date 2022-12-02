592d3fde93d3863630159b524b708b50553c2339
FeelixEbert <felixebert@outlook.com>
2022-12-02 21:21:02 +0100

    first progres on A10_11.6

--- modulhandbuch-webapp/src/app/create-module/create-module.component.ts
+++ modulhandbuch-webapp/src/app/create-module/create-module.component.ts
@@ -3,6 +3,7 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 import { RestApiService } from '../services/rest-api.service';
 import { CollegeEmployee } from '../shared/CollegeEmployee';
 import { Module } from '../shared/module';
+import { ModuleManual } from '../shared/module-manual';
 import { Spo } from '../shared/spo';
 
 @Component({
@@ -13,72 +14,83 @@ import { Spo } from '../shared/spo';
 export class CreateModuleComponent implements OnInit {
   
   display: boolean = false;//false == form hidden | true == form visible
-  newModule!:Module;
+  loaded:number=0;
+  disabled!:boolean[];
 
+  newModule!:Module;
+  
   moduleFormGroup: FormGroup;
 
   profs!:CollegeEmployee[];
   selectedProfs!:[];
-  spos!:Spo[];
+  moduleManuals!:ModuleManual[];
   moduleOwners!:CollegeEmployee[];
+  cycle!:String[];
+  duration!:String[];
+  language!:String[];
+  maternityProtection!:String[];
+  admissionRequirements:String[][]=[];
+  categorys:String[][]=[];
+  segments:String[][]=[];
 
   constructor(private fb: FormBuilder, private restAPI: RestApiService) {
     this.moduleFormGroup = this.fb.group({
       id: null,
-      moduleName: new FormControl(),
-      abbreviation: new FormControl(),
+      moduleName: new FormControl('', [Validators.required]),
+      abbreviation: new FormControl('', [Validators.required]),
       variations: this.fb.array([]),
-
-      cycle: new FormControl(),
-      duration: new FormControl(),
-      moduleOwner: new FormControl(),
-      profs: new FormControl(),
-      language: new FormControl(),
+      cycle: new FormControl('', [Validators.required]),
+      duration: new FormControl('', [Validators.required]),
+      moduleOwner: new FormControl('', [Validators.required]),
+      profs: new FormControl('', [Validators.required]),
+      language: new FormControl('', [Validators.required]),
       usage: new FormControl(),
-      admissionRequirements: new FormControl(),
       knowledgeRequirements: new FormControl(),
       skills: new FormControl(),
       content: new FormControl(),
-      examType: new FormControl(),
+      examType: new FormControl('', [Validators.required]),
       certificates: new FormControl(),
       mediaType: new FormControl(),
       literature: new FormControl(),
-      maternityProtection: new FormControl(),
+      maternityProtection: new FormControl('', [Validators.required]),
     });
   }
   
   ngOnInit(): void {
-    this.profs=[
-      {id:0, firstName:"Volkhard", lastName:"Pfeiffer", title:"Prof.", gender:"Herr", email:"volkhard.pfeiffer@hs-coburg.de"},
-      {id:1, firstName:"Dieter", lastName:"Landes", title:"Prof. Dr.", gender:"Herr", email:"dieter.landes@hs-coburg.de"},
-      {id:2, firstName:"Dieter", lastName:"Wießmann", title:"Prof. Dr.", gender:"Herr", email:"dieter.wissmann@hs-coburg.de"},
-      {id:3, firstName:"Thomas", lastName:"Wieland", title:"Prof. Dr.", gender:"Herr", email:"thomas.wieland@hs-coburg.de"},
-      {id:4, firstName:"Jens", lastName:"Grubert", title:"Prof. Dr.", gender:"Herr", email:"Jens.Grubert@hs-coburg.de"},
-      {id:5, firstName:"Quirin", lastName:"Meyer", title:"Prof. Dr.", gender:"Herr", email:"Quirin.Meyer@hs-coburg.de"},
-      {id:6, firstName:"Florian", lastName:"Mittag", title:"Prof. Dr.", gender:"Herr", email:"Florian.Mittag@hs-coburg.de"},
-      {id:7, firstName:"Jürgen", lastName:"Terpin", title:"Prof. Dr.", gender:"Herr", email:"juergen.terpin@hs-coburg.de"},
-      {id:8, firstName:"Matthias", lastName:"Mörz", title:"Prof. Dr.", gender:"Herr", email:"matthias.moerz@hs-coburg.de"},
-    ]
+    this.restAPI.getCollegeEmployees().subscribe(resp => {
+      this.profs = resp;
+      this.loaded++;
+    });
 
-    this.moduleOwners=[
-      {id:0, firstName:"Volkhard", lastName:"Pfeiffer", title:"Prof.", gender:"Herr", email:"volkhard.pfeiffer@hs-coburg.de"},
-      {id:1, firstName:"Dieter", lastName:"Landes", title:"Prof. Dr.", gender:"Herr", email:"dieter.landes@hs-coburg.de"},
-      {id:2, firstName:"Dieter", lastName:"Wießmann", title:"Prof. Dr.", gender:"Herr", email:"dieter.wissmann@hs-coburg.de"},
-      {id:3, firstName:"Thomas", lastName:"Wieland", title:"Prof. Dr.", gender:"Herr", email:"thomas.wieland@hs-coburg.de"},
-      {id:4, firstName:"Jens", lastName:"Grubert", title:"Prof. Dr.", gender:"Herr", email:"Jens.Grubert@hs-coburg.de"},
-      {id:5, firstName:"Quirin", lastName:"Meyer", title:"Prof. Dr.", gender:"Herr", email:"Quirin.Meyer@hs-coburg.de"},
-      {id:6, firstName:"Florian", lastName:"Mittag", title:"Prof. Dr.", gender:"Herr", email:"Florian.Mittag@hs-coburg.de"},
-      {id:7, firstName:"Jürgen", lastName:"Terpin", title:"Prof. Dr.", gender:"Herr", email:"juergen.terpin@hs-coburg.de"},
-      {id:8, firstName:"Matthias", lastName:"Mörz", title:"Prof. Dr.", gender:"Herr", email:"matthias.moerz@hs-coburg.de"},
-    ]
+    this.restAPI.getCollegeEmployees().subscribe(resp => {
+      this.moduleOwners = resp;
+      this.loaded++;
+    });
 
-    this.spos=[
-      {id:0, link:"https://www.hs-coburg.de/fileadmin/hscoburg/Amtsblatt/2014/SPO_B_BW_6.pdf",startDate:"2014-08-01", endDate:"2020-08-01", course:"B BW", degree:"Bachelor" },
-      {id:1, link:"https://www.hs-coburg.de/fileadmin/hscoburg/Amtsblatt/2021/SPO_B_IW_3.pdf",startDate:"2021-11-25", endDate:"2022-12-31", course:"B IW", degree:"Bachelor"  },
-      {id:2, link:"https://www.hs-coburg.de/fileadmin/hscoburg/Amtsblatt/2021/SPO_B_SA_9.pdf",startDate:"2014-12-23", endDate:"2022-12-31", course:"B SA", degree:"Bachelor"  },
-      {id:3, link:"https://www.hs-coburg.de/fileadmin/hscoburg/Amtsblatt/2022/SPO__B__ADT_2022.pdf",startDate:"2022-05-24", endDate:"2022-08-01", course:" B ADT", degree:"Bachelor"  },
-      {id:4, link:"https://www.hs-coburg.de/fileadmin/hscoburg/Amtsblatt/2021/SPO_B_ZT.pdf",startDate:"2021-05-06", endDate:"2022-01-01", course:"B ZT", degree:"Bachelor"  },
-    ]
+    this.restAPI.getModuleManuals().subscribe(resp => {
+      this.moduleManuals = resp;
+      this.loaded++;
+    });
+
+    this.restAPI.getCycles().subscribe(resp => {
+      this.cycle = resp;
+      this.loaded++;
+    });
+
+    this.restAPI.getDurations().subscribe(resp => {
+      this.duration = resp;
+      this.loaded++;
+    });
+
+    this.restAPI.getMaternityProtections().subscribe(resp => {
+      this.maternityProtection = resp;
+      this.loaded++;
+    });
+
+    this.restAPI.getLanguages().subscribe(resp => {
+      this.language = resp;
+      this.loaded++;
+    });
 
     this.addVariation();
   }
@@ -100,6 +112,56 @@ export class CreateModuleComponent implements OnInit {
     }
   }
 
+  updateSpoForm(id: string, i:number) {
+
+    this.restAPI.getModuleTypes(parseInt(id)).subscribe(resp => {
+        this.categorys[i]=(resp);
+    });
+
+    this.restAPI.getRequirements(parseInt(id)).subscribe(resp => {
+      this.admissionRequirements[i]= resp;
+    });
+
+    this.restAPI.getSegments(parseInt(id)).subscribe(resp => {
+      for(let j=0;j<resp.length;i++){
+        this.segments[i]=resp;
+      }
+    });
+
+    this.disabled[i]=false;
+  }
+
+  get variations(){
+    return (this.moduleFormGroup.get("variations") as FormArray);
+  }
+
+  addVariation() {
+    this.variations.push(
+      this.fb.group({
+        moduleManual: new FormControl('', [Validators.required]),
+        ects: new FormControl('', [Validators.required]),
+        sws: new FormControl('', [Validators.required]),
+        workLoad: new FormControl('', [Validators.required]),
+        semester: new FormControl('', [Validators.required]),
+        moduleCategory: new FormControl('', [Validators.required]),
+        admissionRequirements: new FormControl('', [Validators.required]),
+        segment:new FormControl('', [Validators.required]),
+      })
+    );
+    this.disabled.push(true);//Test
+  }
+
+  deleteVariation(index:number){
+    if(this.variations.length >1){
+      this.variations.removeAt(index);
+      delete this.disabled[index];//testing
+      delete this.categorys[index]//testing
+      delete this.admissionRequirements[index]//testing
+      delete this.segments[index]//Testing
+    }else{
+      window.alert("Es muss mindestens eine Variation vorhanden sein")
+    }
+  }
 
   showDialog() {//make form visible
     this.display = true;
@@ -111,30 +173,5 @@ export class CreateModuleComponent implements OnInit {
 
   resetForm(){
     this.moduleFormGroup.reset();
-  }
-
-  get variations(){
-    return (this.moduleFormGroup.get("variations") as FormArray);
-  }
-
-  addVariation() {
-    this.variations.push(
-      this.fb.group({
-        spo: new FormControl('', [Validators.required]),
-        ects: new FormControl('', [Validators.required]),
-        sws: new FormControl('', [Validators.required]),
-        workLoad: new FormControl('', [Validators.required]),
-        semester: new FormControl('', [Validators.required]),
-        category: new FormControl('', [Validators.required]),
-      })
-    );
-  }
-
-  deleteVariation(index:number){
-    if(this.variations.length >1){
-      this.variations.removeAt(index);
-    }else{
-      window.alert("Es muss mindestens eine Variation vorhanden sein")
-    }
   }
 }
