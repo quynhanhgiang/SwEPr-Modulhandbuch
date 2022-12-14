8742dd1d20de605ee135e2561986443d38d100cc
Simon <116567538+simonstudentcoburg@users.noreply.github.com>
2022-12-14 12:30:46 +0100

    feat: changed tests

--- modulhandbuch-test/src/test/java/a1/a1_systemtest.java
+++ modulhandbuch-test/src/test/java/a1/a1_systemtest.java
@@ -81,87 +81,7 @@ public class a1_systemtest {
 	}
 
 	@Test
-	public void S_D_A1T01() {
-		boolean result = true;
-		openFormular();
-
-		Date date = new Date();
-		String d = date.toString().replaceAll(" ", "_").replaceAll(":", "_");
-
-		String testMail = "Test_Mail_" + d + "@test.de";
-		String testFirstName = "Test_First_Name_" + d;
-		String testLastName = "Test_Last_Name_" + d;
-
-		Select gender = new Select(driver.findElement(By.xpath("//*[@id='select-create-employee-gender']")));
-		gender.selectByVisibleText("Herr");
-
-		driver.findElement(By.xpath("//*[@id='input-first-name']")).sendKeys(testFirstName);
-		driver.findElement(By.xpath("//*[@id='input-last-name']")).sendKeys(testLastName);
-
-		driver.findElement(By.xpath("//*[@id='input-email']")).sendKeys(testMail);
-
-		driver.findElement(By.xpath("/html/body/app-root/main/div/div/app-get-college-employees/div/app-create-college-employee/p-dialog/div/div/div[3]/form/div[1]/div[2]/p-multiselect/div/div[2]/div")).click();
-		driver.findElement(By.xpath("/html/body/app-root/main/div/div/app-get-college-employees/div/app-create-college-employee/p-dialog/div/div/div[3]/form/div[1]/div[2]/p-multiselect/div/div[4]/div[2]/ul/p-multiselectitem[1]/li/span")).click();
-		driver.findElement(By.xpath("/html/body/app-root/main/div/div/app-get-college-employees/div/app-create-college-employee/p-dialog/div/div/div[3]/form/div[1]/div[2]/p-multiselect/div/div[4]/div[1]/button/span")).click();
-
-		driver.findElement(By.xpath("//*[@id='btn-submit-close']")).click();
-		
-		try {
-			driver.findElement(By.xpath("/html/body/app-root/main/div/div/app-get-college-employees/div/p-dataview/div/p-paginator/div")).click();
-		} catch(ElementClickInterceptedException e) {
-			
-		}
-		
-		try {
-			driver.findElement(By.name("Prof. " + testFirstName + " " + testLastName));
-			driver.findElement(By.name(testMail));
-		} catch (NoSuchElementException noSuchElement) {
-			result = false;
-		}
-		
-		Assert.assertEquals(result, true);
-	}
-
-	@Test
-	public void S_D_A1T02() {
-		boolean result = true;
-		openFormular();
-
-		Date date = new Date();
-		String d = date.toString().replaceAll(" ", "_").replaceAll(":", "_");
-
-		String testMail = "Test_Mail_" + d + "@test.de";
-		String testFirstName = "Test_First_Name_" + d;
-		String testLastName = "Test_Last_Name_" + d;
-
-		Select gender = new Select(driver.findElement(By.xpath("//*[@id='select-create-employee-gender']")));
-		gender.selectByVisibleText("Herr");
-
-		driver.findElement(By.xpath("//*[@id='input-first-name']")).sendKeys(testFirstName);
-		driver.findElement(By.xpath("//*[@id='input-last-name']")).sendKeys(testLastName);
-
-		driver.findElement(By.xpath("//*[@id='input-email']")).sendKeys(testMail);
-
-		driver.findElement(By.xpath("//*[@id='btn-submit-close']")).click();
-
-		try {
-			driver.findElement(By.xpath("/html/body/app-root/main/div/div/app-get-college-employees/div/p-dataview/div/p-paginator/div")).click();
-		} catch(ElementClickInterceptedException e) {
-			
-		}
-		
-		try {
-			driver.findElement(By.name("Prof. " + testFirstName + " " + testLastName));
-			driver.findElement(By.name(testMail));
-		} catch (NoSuchElementException noSuchElement) {
-			result = false;
-		}
-		
-		Assert.assertEquals(result, true);
-	}
-
-	@Test
-	public void S_D_A1T03() throws SQLException, ClassNotFoundException {
+	public void S_D_A1T01() throws SQLException, ClassNotFoundException {
 		boolean result = false;
 		List<Boolean> resultList = new ArrayList<Boolean>();
 		openFormular();
@@ -218,7 +138,7 @@ public class a1_systemtest {
 	}
 
 	@Test
-	public void S_D_A1T04() throws SQLException, ClassNotFoundException {
+	public void S_D_A1T02() throws SQLException, ClassNotFoundException {
 		boolean result = false;
 		List<Boolean> resultList = new ArrayList<Boolean>();
 		openFormular();
