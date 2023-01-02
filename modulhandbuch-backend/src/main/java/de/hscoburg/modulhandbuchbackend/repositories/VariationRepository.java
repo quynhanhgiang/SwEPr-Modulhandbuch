9efb067530328ae3f807b3adfb78ce0ec637b286
Ganko Pi <ganko-pi@outlook.de>
2023-01-02 18:43:18 +0100

    A11.2 fix backend: no direct reference to ModuleManual in Module, now access per Variation

--- modulhandbuch-backend/src/main/java/de/hscoburg/modulhandbuchbackend/repositories/VariationRepository.java
+++ modulhandbuch-backend/src/main/java/de/hscoburg/modulhandbuchbackend/repositories/VariationRepository.java
@@ -1,8 +1,13 @@
 package de.hscoburg.modulhandbuchbackend.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import de.hscoburg.modulhandbuchbackend.model.entities.ModuleManualEntity;
 import de.hscoburg.modulhandbuchbackend.model.entities.VariationEntity;
 
 public interface VariationRepository extends JpaRepository<VariationEntity, Integer> {
+	List<VariationEntity> findByModuleManual(ModuleManualEntity moduleManual);
+	List<VariationEntity> findByModuleManualNot(ModuleManualEntity moduleManual);
 }
