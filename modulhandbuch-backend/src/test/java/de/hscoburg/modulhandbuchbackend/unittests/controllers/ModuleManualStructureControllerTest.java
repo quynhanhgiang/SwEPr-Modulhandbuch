da76d2e098da8383cf8404644971ca39a1c9116e
Ganko Pi <ganko-pi@outlook.de>
2023-01-03 18:42:24 +0100

    A10.6 test backend: tests for structure of module manuals

--- modulhandbuch-backend/src/test/java/de/hscoburg/modulhandbuchbackend/unittests/controllers/ModuleManualStructureControllerTest.java
+++ modulhandbuch-backend/src/test/java/de/hscoburg/modulhandbuchbackend/unittests/controllers/ModuleManualStructureControllerTest.java
@@ -1,5 +1,7 @@
 package de.hscoburg.modulhandbuchbackend.unittests.controllers;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -8,6 +10,7 @@ import java.util.function.BiFunction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.AdditionalAnswers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -15,6 +18,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import de.hscoburg.modulhandbuchbackend.controllers.ModuleManualStructureController;
 import de.hscoburg.modulhandbuchbackend.dto.StructureDTO;
+import de.hscoburg.modulhandbuchbackend.exceptions.ModuleManualNotFoundException;
+import de.hscoburg.modulhandbuchbackend.model.entities.ModuleManualEntity;
 import de.hscoburg.modulhandbuchbackend.repositories.ModuleManualRepository;
 import de.hscoburg.modulhandbuchbackend.repositories.SectionRepository;
 import de.hscoburg.modulhandbuchbackend.repositories.TypeRepository;
@@ -37,6 +42,9 @@ public class ModuleManualStructureControllerTest {
 	@InjectMocks
 	private ModuleManualStructureController moduleManualStructureControllerWithMocks;
 
+	@Mock
+	private ModuleManualEntity mockModuleManualEntity;
+
 	@Test
 	public void testPutMappings() {
 		@AllArgsConstructor
@@ -46,9 +54,13 @@ public class ModuleManualStructureControllerTest {
 			BiFunction<List<StructureDTO>, Integer, List<StructureDTO>> method;
 			List<StructureDTO> structure;
 			Integer id;
+			@EqualsAndHashCode.Exclude
 			boolean expectedToThrowException;
+			@EqualsAndHashCode.Exclude
 			Class<? extends Exception> expectedException;
+			@EqualsAndHashCode.Exclude
 			String expectedExceptionMessage;
+			@EqualsAndHashCode.Exclude
 			List<StructureDTO> expectedStructureDTOList;
 		}
 
@@ -56,33 +68,66 @@ public class ModuleManualStructureControllerTest {
 		newElement.setId(null);
 		newElement.setValue("New element");
 
-		// StructureDTO
+		StructureDTO existingElement1 = new StructureDTO();
+		existingElement1.setId(1);
+		existingElement1.setValue("Existing element 1");
 
-		Set<TestParameters> testData = Set.of(
-			// // oneModulePlan
-			// new TestParameters((id, structure) -> this.moduleManualDocumentControllerWithMocks.replaceSegments(id, structure), 0, false, null, null, this.fileInfoModulePlan),
-			// new TestParameters((id) -> this.moduleManualDocumentControllerWithMocks.oneModulePlan(id), 1, false, null, null, new FileInfoDTO()),
-			// new TestParameters((id) -> this.moduleManualDocumentControllerWithMocks.oneModulePlan(id), 2, true, RuntimeException.class, "Id 2 for module manual not found.", null),
-			// new TestParameters((id) -> this.moduleManualDocumentControllerWithMocks.oneModulePlan(id), -1, true, RuntimeException.class, "Id -1 for module manual not found.", null),
-			// new TestParameters((id) -> this.moduleManualDocumentControllerWithMocks.oneModulePlan(id), null, true, IllegalArgumentException.class, null, null),
+		StructureDTO existingElement2 = new StructureDTO();
+		existingElement2.setId(2);
+		existingElement2.setValue("Existing element 2");
 
-			// // onePreliminaryNote
-			// new TestParameters((id) -> this.moduleManualDocumentControllerWithMocks.onePreliminaryNote(id), 0, false, null, null, this.fileInfoPreliminaryNote),
-			// new TestParameters((id) -> this.moduleManualDocumentControllerWithMocks.onePreliminaryNote(id), 1, false, null, null, new FileInfoDTO()),
-			// new TestParameters((id) -> this.moduleManualDocumentControllerWithMocks.onePreliminaryNote(id), 2, true, RuntimeException.class, "Id 2 for module manual not found.", null),
-			// new TestParameters((id) -> this.moduleManualDocumentControllerWithMocks.onePreliminaryNote(id), -1, true, RuntimeException.class, "Id -1 for module manual not found.", null),
-			// new TestParameters((id) -> this.moduleManualDocumentControllerWithMocks.onePreliminaryNote(id), null, true, IllegalArgumentException.class, null, null)
+		List<StructureDTO> structure = Arrays.asList(
+			existingElement1,
+			newElement,
+			existingElement2,
+			null
 		);
 
-		// // initially no module manuals are present
-		// Mockito.when(this.mockModuleManualRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
-		// // IllegalArgumentException if id is null
-		// Mockito.when(this.mockModuleManualRepository.findById(null)).thenThrow(new IllegalArgumentException());
-		// // TODO
-		// // // module manual with id 0 is present and is linked to documents
-		// // Mockito.when(this.mockModuleManualRepository.findById(0)).thenReturn(Optional.of(this.moduleManualEntityWithContent));
-		// // // module manual with id 1 is present but is not linked to documents
-		// // Mockito.when(this.mockModuleManualRepository.findById(1)).thenReturn(Optional.of(this.mockModuleManualEntity));
+		List<StructureDTO> expectedResultForStructure = Arrays.asList(
+			existingElement1,
+			newElement,
+			existingElement2,
+			null
+		);
+
+		Set<TestParameters> testData = Set.of(
+			// replaceSegments
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), new LinkedList<>(structure), 0, false, null, null, expectedResultForStructure),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), new LinkedList<>(structure), 1, true, ModuleManualNotFoundException.class, "Module manual with id 1 not found.", null),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), new LinkedList<>(structure), -1, true, ModuleManualNotFoundException.class, "Module manual with id -1 not found.", null),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), new LinkedList<>(structure), null, true, IllegalArgumentException.class, null, null),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), new LinkedList<>(), 0, false, null, null, new LinkedList<>()),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), new LinkedList<>(), 1, true, ModuleManualNotFoundException.class, "Module manual with id 1 not found.", null),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), new LinkedList<>(), -1, true, ModuleManualNotFoundException.class, "Module manual with id -1 not found.", null),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), new LinkedList<>(), null, true, IllegalArgumentException.class, null, null),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), null, 0, false, null, null, null),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), null, 1, true, ModuleManualNotFoundException.class, "Module manual with id 1 not found.", null),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), null, -1, true, ModuleManualNotFoundException.class, "Module manual with id -1 not found.", null),
+			new TestParameters((segments, id) -> this.moduleManualStructureControllerWithMocks.replaceSegments(segments, id), null, null, true, IllegalArgumentException.class, null, null),
+
+			// replaceModuleTypes
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), new LinkedList<>(structure), 0, false, null, null, expectedResultForStructure),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), new LinkedList<>(structure), 1, true, ModuleManualNotFoundException.class, "Module manual with id 1 not found.", null),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), new LinkedList<>(structure), -1, true, ModuleManualNotFoundException.class, "Module manual with id -1 not found.", null),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), new LinkedList<>(structure), null, true, IllegalArgumentException.class, null, null),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), new LinkedList<>(), 0, false, null, null, new LinkedList<>()),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), new LinkedList<>(), 1, true, ModuleManualNotFoundException.class, "Module manual with id 1 not found.", null),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), new LinkedList<>(), -1, true, ModuleManualNotFoundException.class, "Module manual with id -1 not found.", null),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), new LinkedList<>(), null, true, IllegalArgumentException.class, null, null),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), null, 0, false, null, null, null),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), null, 1, true, ModuleManualNotFoundException.class, "Module manual with id 1 not found.", null),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), null, -1, true, ModuleManualNotFoundException.class, "Module manual with id -1 not found.", null),
+			new TestParameters((moduleTypes, id) -> this.moduleManualStructureControllerWithMocks.replaceModuleTypes(moduleTypes, id), null, null, true, IllegalArgumentException.class, null, null)
+		);
+
+		// initially no module manuals are present
+		Mockito.when(this.mockModuleManualRepository.findById(Mockito.anyInt())).thenReturn(Optional.empty());
+		// IllegalArgumentException if id is null
+		Mockito.when(this.mockModuleManualRepository.findById(null)).thenThrow(new IllegalArgumentException());
+		// module manual with id 0 is present
+		Mockito.when(this.mockModuleManualRepository.findById(0)).thenReturn(Optional.of(this.mockModuleManualEntity));
+		
+		Mockito.when(this.mockModuleManualStructureService.updateStructure(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenAnswer(AdditionalAnswers.returnsFirstArg());
 
 		for (TestParameters testParameters : testData) {
 			if (testParameters.expectedToThrowException) {
