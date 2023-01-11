cdf6ef35f06cffff347db542d5f6913cebcc237f
Simon <116567538+simonstudentcoburg@users.noreply.github.com>
2023-01-11 09:45:04 +0100

    feat: Systemtest A6

--- modulhandbuch-test/src/test/java/A6/A6_systemtest.java
+++ modulhandbuch-test/src/test/java/A6/A6_systemtest.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -51,8 +52,8 @@ public class A6_systemtest {
 		try {
 			wait.until(ExpectedConditions.elementToBeClickable(By.id("btn-hamburger")));
 			driver.findElement(By.id("btn-hamburger")).click();
-			wait.until(ExpectedConditions.elementToBeClickable(By.id("a-module-manuals")));
-			driver.findElement(By.id("a-module-manuals")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.id("a-module-management")));
+			driver.findElement(By.id("a-module-management")).click();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -61,8 +62,8 @@ public class A6_systemtest {
 
 	public void openFormular() {
 		try {
-			wait.until(ExpectedConditions.elementToBeClickable(By.id("btn-create-module-manual")));
-			driver.findElement(By.id("btn-create-module-manual")).click();
+			wait.until(ExpectedConditions.elementToBeClickable(By.id("btn-create-module")));
+			driver.findElement(By.id("btn-create-module")).click();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			System.out.println("Button to open the form does not work");
@@ -77,6 +78,38 @@ public class A6_systemtest {
 			"read_only_user_local_host", "car_tree_moon"
 		);
 		return connection;
+	}
+
+	@Test
+	public void S_D_A4T01() {
+		boolean result = false;
+        List<Boolean> resultList = new ArrayList<Boolean>();
+
+		String module_name = "Systemtest_A6";
+		String changed = "Testfield has been changed";
+		String changed_re = "Systemtest_A6";
+
+		driver.findElement(By.xpath("/html/body/app-root/main/div/div/app-get-modules/div/p-table/div/div/table/thead/tr[2]/th[1]/p-columnfilter/div/p-columnfilterformelement/input")).sendKeys(module_name);
+		driver.findElement(By.xpath("/html/body/app-root/main/div/div/app-get-modules/div/p-table/div/div/table/thead/tr[2]/th[1]/p-columnfilter/div/p-columnfilterformelement/input")).sendKeys(Keys.RETURN);
+
+		driver.findElement(By.xpath("//td[.='" + module_name + "']")).click();
+
+		driver.findElement(By.xpath("//*[@id='btn-edit-module']")).click();
+		driver.findElement(By.xpath("//*[@id='input-create-module-media-type']")).clear();
+		driver.findElement(By.xpath("//*[@id='input-create-module-media-type']")).sendKeys(changed);
+
+		//TODO: Test every field
+
+		driver.findElement(By.xpath("//*[@id='btn-submit-close']")).click();
+
+		resultList.add(driver.getPageSource().contains(changed));
+
+		driver.findElement(By.xpath("//*[@id='btn-edit-module']")).click();
+		driver.findElement(By.xpath("//*[@id='input-create-module-media-type']")).clear();
+		driver.findElement(By.xpath("//*[@id='input-create-module-media-type']")).sendKeys(changed_re);
+
+		result = !resultList.contains((Boolean) false);
+        Assert.assertEquals(result, true);
 	}
     
 }
